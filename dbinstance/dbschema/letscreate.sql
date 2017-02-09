efc52156f95f1b38cc95af9e689a7e499c227fbb
Sanborn <sanborn.sen@gmail.com>
2017-02-10 00:00:23 +0530

    User schema added

--- dbinstance/dbschema/letscreate.sql
+++ dbinstance/dbschema/letscreate.sql
@@ -2,8 +2,8 @@
 -- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
--- Host: 172.17.0.2:3306
--- Generation Time: Feb 09, 2017 at 02:42 PM
+-- Host: 172.17.0.3:3306
+-- Generation Time: Feb 09, 2017 at 06:28 PM
 -- Server version: 10.1.20-MariaDB-1~jessie
 -- PHP Version: 5.6.9-1+deb.sury.org~trusty+2
 
@@ -41,13 +41,26 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `name`, `description`, `start_date`, `end_date`, `creator_id`, `banner_url`) VALUES
-(1, 'THE EYESLICER SEASON ONE', NULL, '2017-04-21 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-1.png');
-INSERT INTO `events` (`id`, `name`, `description`, `start_date`, `end_date`, `creator_id`, `banner_url`) VALUES
-(2, 'Peter Evans and Taylor Ho Bynum', NULL, '2017-04-23 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-2.png');
-INSERT INTO `events` (`id`, `name`, `description`, `start_date`, `end_date`, `creator_id`, `banner_url`) VALUES
-(3, 'Second Sundays', NULL, '2017-04-24 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-3.png');
-INSERT INTO `events` (`id`, `name`, `description`, `start_date`, `end_date`, `creator_id`, `banner_url`) VALUES
+(1, 'THE EYESLICER SEASON ONE', NULL, '2017-04-21 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-1.png'),
+(2, 'Peter Evans and Taylor Ho Bynum', NULL, '2017-04-23 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-2.png'),
+(3, 'Second Sundays', NULL, '2017-04-24 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-3.png'),
 (4, 'Building and Improvising with Analog Circuitry', NULL, '2017-04-25 00:00:00', '2017-04-30 00:00:00', NULL, 'images/events/event-4.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` text NOT NULL,
+  `display_picture` int(11) NOT NULL COMMENT 'This is a foreign key to media table',
+  `activity_score` int(100) NOT NULL,
+  `last_loggedin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -60,6 +73,13 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,7 +87,12 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
